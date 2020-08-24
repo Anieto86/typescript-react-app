2fe88bb535a4f3f31af4a41fe4410703ff3d61cc
Alvaro Nieto <60838509+Anieto86@users.noreply.github.com>
2020-08-24 13:33:56 +0930

    add remove buttom

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {  useState } from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
 
@@ -18,19 +18,32 @@ function App(): JSX.Element {
   //New state for new list of taskSSSS
   const [tasks, setTasks] = useState<ITask[]>([])
 
-  // Function for submit
+  //! Function for submit
   const handleSubmit = (e: FormElement) => {
     e.preventDefault();
     addTask(newTask)
     setNewTask("");
   }
 
-  //This function takes the name of the new Task 
+  //!This function takes the name of the new Task 
   const addTask = (name: string) => {
     //spare operato | vas a tomar la tareas y vas a agregar la nueva tarea
-    const newTasks = [...tasks, { name: name, done: false }]
+    const newTasks = [...tasks, { name: name, done: false }];
     setTasks(newTasks);
+  }
 
+  //!Fuction para Alterar el estado el estado de true to false = si es (?)| caso contrario (:) 
+  const toggleDoneTask = (index: number) => {
+    const newTasks: ITask[] = [...tasks];
+    newTasks[index].done = !newTasks[index].done;
+    setTasks(newTasks)
+  }
+
+  //!Function para eliminar tareas | SPLICE method
+  const removeTask = (index: number) => {
+    const newTasks: ITask[] = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks)
   }
 
   return (
@@ -39,20 +52,35 @@ function App(): JSX.Element {
         <div className="col-md-6 offset-md-3">
           <div className="card">
             <div className="card-body">
-              <form onSubmit={handleSubmit}>
-                <input className="form-control" type="text" name="" id="" placeholder="note"
+              <form onSubmit={handleSubmit} >
+                <input className="form-control" type="text" placeholder="note" autoFocus
+                  // autofocus | pone el cursor en el imput
                   //                                          para representar la nueva tarea al inicio esta en blanco y luego se actualiza
                   onChange={e => setNewTask(e.target.value)} value={newTask} />
-                <button className=" btn-warning" >Save </button>
+                <button className="btn btn-warning btn-block mt-2" >Save </button>
               </form>
             </div>
           </div>
-          {tasks.map((t: ITask, index: number) => {
-              return <h1 key={index}>{t.name}</h1>})}
+          {tasks.map((t: ITask, index: number) => (
+            <div className="card card-body mt-2" key={index}>
+              <h2 style={{ textDecoration: t.done ? 'line-through' : '' }}>{t.name}</h2>
+              <div className = "">
+                <button className="btn btn-info" style={{ alignItems: "center"}} onClick={() => toggleDoneTask(index)} >
+                  {t.done ? "✔" : "✗"}
+                </button>
+                <button className="btn btn-danger"  style={{ alignItems: "center"}} onClick={() => removeTask(index)}>
+                  🗑
+                 </button>
+              </div>
+            </div>
+          ))};
         </div>
       </div>
-      </div>
+    </div>
   );
 }
 
 export default App;
+
+
+
