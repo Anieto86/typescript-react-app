2447e8df8f400430e369592953dd60e2451456df
Anieto86 <alvaro16nieto@gmail.com>
2023-09-11 22:05:49 +0930

    testing mobx

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 import './App.css';
 import { Task } from './Components/Task';
+import { Roster } from './Components/Roster';
 
 function App(): JSX.Element {
   return (
     <div className="container p-4">
+      <div className="col-md-6 offset-md-3">
+        <Roster />
+      </div>
       <div className="row">
         <div className="col-md-6 offset-md-3">
           <Task />
