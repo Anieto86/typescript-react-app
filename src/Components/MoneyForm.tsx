3f480126021e737d6b471db8b0ec68aedcd463bb
Anieto86 <alvaro16nieto@gmail.com>
2023-09-21 09:27:08 +0930

    fix and observale in JS object using toJS form mobx

--- src/Components/MoneyForm.tsx
+++ src/Components/MoneyForm.tsx
@@ -1,29 +1,55 @@
-import React, { useState } from 'react';
+import { action, computed, observable, toJS } from 'mobx';
+import { observer } from 'mobx-react-lite';
 
-export const MoneyForm = () => {
-  const [total, setTotal] = useState<number>(0);
-  const [years, setYears] = useState<number>(0);
-  const [salary, setSalary] = useState<number>(0);
+type FormState = {
+  total: number;
+  years: number;
+  salary: number;
+};
 
+const formState: FormState = observable({
+  total: 0,
+  years: 0,
+  salary: 0,
+});
+
+// const calculateTotal = action((formState: FormState) => {
+//   formState.total = formState.years * formState.salary;
+// });
+
+const totalValue = computed(() => formState.salary * formState.years);
+
+console.log(toJS(totalValue));
+
+export const MoneyForm = observer(() => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <h1 style={{ marginBottom: 0 }}>Money Talks</h1>
-      <p>Total:{total}</p>
+      <h1 style={{ textAlign: 'center' }}>Money Talks</h1>
+      {/* <h2 style={{ textAlign: 'center', padding: 6 }}>
+        Total:{formState.total}
+      </h2> */}
+      <h2 style={{ textAlign: 'center', padding: 6 }}>
+        Total:{toJS(totalValue)}
+      </h2>
       <input
         type="number"
         placeholder="Years..."
         style={{ height: '40px' }}
-        onChange={(e) => setYears(+e.target.value)}
+        onChange={action((e) => {
+          formState.years = Number(e.target.value);
+        })}
       />
       <input
         type="number"
         placeholder="Years Salary..."
         style={{ height: '40px' }}
-        onChange={(e) => setSalary(+e.target.value)}
+        onChange={action((e) => {
+          formState.salary = Number(e.target.value);
+        })}
       />
-      <button type="button" onClick={() => setTotal(years * salary)}>
+      {/* <button type="button" onClick={() => calculateTotal(formState)}>
         Calculate Total
-      </button>
+      </button> */}
     </div>
   );
-};
+});
--- src/Components/Roster.tsx
+++ src/Components/Roster.tsx
@@ -8,43 +8,47 @@ const elDiego = new Athlete('El Diego', 60, 30);
 
 export const Roster = observer(() => {
   return (
-    <table className="table table-hover">
-      <thead>
-        <tr className="table-info">
-          <th scope="col">Name</th>
-          <th scope="col">Age</th>
-          <th scope="col">Teams</th>
-          <th scope="col">Is it their birthday?</th>
-          <th>Trade Form</th>
+    <>
+      {' '}
+      <h1 style={{ textAlign: 'center' }}>Roster</h1>
+      <table className="table table-hover">
+        <thead>
+          <tr className="table-info">
+            <th scope="col">Name</th>
+            <th scope="col">Age</th>
+            <th scope="col">Teams</th>
+            <th scope="col">Is it their birthday?</th>
+            <th>Trade Form</th>
 
-          <th scope="col">Salary</th>
-        </tr>
-      </thead>
+            <th scope="col">Salary</th>
+          </tr>
+        </thead>
 
-      {[messi, elDiego].map((athlete) => {
-        return (
-          <tbody key={athlete.name}>
-            <tr>
-              <td>{athlete.name}</td>
-              <td>{athlete.age}</td>
-              <td>{athlete.teamHistory}</td>
-              <td>
-                <TradeForm athlete={athlete} />
-              </td>
-              <td>
-                <button
-                  type="button"
-                  style={{ width: '100%' }}
-                  onClick={() => athlete.wishHappyBirthday()}
-                >
-                  Wish happy birthday 🎊
-                </button>
-              </td>
-              <td>{athlete.salary}</td>
-            </tr>
-          </tbody>
-        );
-      })}
-    </table>
+        {[messi, elDiego].map((athlete) => {
+          return (
+            <tbody key={athlete.name}>
+              <tr>
+                <td>{athlete.name}</td>
+                <td>{athlete.age}</td>
+                <td>{athlete.teamHistory}</td>
+                <td>
+                  <TradeForm athlete={athlete} />
+                </td>
+                <td>
+                  <button
+                    type="button"
+                    style={{ width: '100%' }}
+                    onClick={() => athlete.wishHappyBirthday()}
+                  >
+                    Wish happy birthday 🎊
+                  </button>
+                </td>
+                <td>{athlete.salary}</td>
+              </tr>
+            </tbody>
+          );
+        })}
+      </table>
+    </>
   );
 });
